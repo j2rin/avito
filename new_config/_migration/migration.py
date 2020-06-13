a1180d9527d8439fae44e5bb03ac2496805cc575
Danila Lenkov <dlenkov@avito.ru>
2020-06-13 12:37:35 +0300

    AB-613 duplicates handling

--- new_config/_migration/migration.py
+++ new_config/_migration/migration.py
@@ -1,27 +1,28 @@
-from models import *
+from models import Metric
 from pathlib import Path
-from typing import TextIO
+from typing import TextIO, List
 from ruamel.yaml import safe_load
 import os
+from itertools import groupby
+import operator
 
 
 CONFIG_PATH = Path.cwd()
 MIGRATED_PATH = CONFIG_PATH / 'migrated'
 
 
-def write_metrics_type_batch(metrics: Set[Metric], typ: str, fil: TextIO, ratio=False):
+def write_metrics_type_batch(metrics: List[Metric], typ: str, fil: TextIO, ratio=False):
+    metrics = sorted(metrics, key=lambda x: (str(x.key), x.name))
     if ratio:
-        metrics = sorted(metrics, key=lambda x: (str(x.key), x.name))
         lines = [m.yaml_repr.strip() + '\n' for m in metrics]
     else:
         fil.write(f'metric.{typ}:\n')
-        metrics = sorted(metrics, key=lambda x: (str(x.key), x.name))
         lines = [m.yaml_repr for m in metrics]
     fil.writelines(lines)
     fil.write('\n')
 
 
-def write_metrics_to_file(metrics: Set[Metric], source: str, extra_path):
+def write_metrics_to_file(metrics: List[Metric], source: str, extra_path):
     source_map = {
         'new_performance': 'perf_mobile',
         'web_performance': 'perf_web',
@@ -29,7 +30,8 @@ def write_metrics_to_file(metrics: Set[Metric], source: str, extra_path):
     source = source_map.get(source, source)
     os.makedirs(MIGRATED_PATH / extra_path, exist_ok=True)
     filepath = MIGRATED_PATH / extra_path / f'{source}.yaml'
-    index_by_type = MetricIndex(metrics).by_type
+    types_sorted = ['counter', 'uniq', 'ratio']
+    by_type = groupby(sorted(metrics, key=lambda x: types_sorted.index(x.type)), key=operator.attrgetter('type'))
     with open(filepath, 'w') as f:
         headers_dict = {
             'buyer_stream': 'ss_header.yaml',
@@ -38,18 +40,16 @@ def write_metrics_to_file(metrics: Set[Metric], source: str, extra_path):
         if source in headers_dict:
             with open(headers_dict[source], 'r') as h:
                 f.write(h.read())
-        types = ['counter', 'uniq', 'ratio']
-        for t in types:
-            m = index_by_type.get(t)
+        for t, m in by_type:
             ratio = extra_path == 'ratio'
             if m:
-                write_metrics_type_batch(m, t, f, ratio)
+                write_metrics_type_batch(list(m), t, f, ratio)
 
 
-def write_all_metrics_to_files(metrics: Set[Metric], extra_path=''):
-    index = MetricIndex(metrics)
-    for s, m in index.by_source.items():
-        write_metrics_to_file(m, s, extra_path)
+def write_all_metrics_to_files(metrics: List[Metric], extra_path=''):
+    by_source = groupby(sorted(metrics, key=operator.attrgetter('source')), key=operator.attrgetter('source'))
+    for s, m in by_source:
+        write_metrics_to_file(list(m), s, extra_path)
 
 
 def write_sources(sources: List[str]):
