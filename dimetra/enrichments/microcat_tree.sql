df5bfe7edf9e4c9db2212a15f4e05cdc9a39f067
dakhaytin <dakhaytin@avito.ru>
2022-10-17 17:53:06 +0300

    OLAP-14420 add new enrichments

--- dimetra/enrichments/microcat_tree.sql
+++ dimetra/enrichments/microcat_tree.sql
@@ -4,17 +4,19 @@ select
     first(lc.logical_category_id, cm.logical_category_id)     as logical_category_id,
     lc.logical_param1_id,
     lc.logical_param2_id,
+    lc.logcat_id,
     cm.category_id                                            as category_id,
     cm.subcategory_id                                         as subcategory_id,
     cm.Param1_microcat_id                                     as param1_id,
     cm.Param2_microcat_id                                     as param2_id,
     cm.Param3_microcat_id                                     as param3_id,
     cm.Param4_microcat_id                                     as param4_id
-from :fact_table                               t
+from :fact_table  t
 left join infomodel.current_infmquery_category ic on ic.infmquery_id = t.infmquery_id
 left join dma.current_logical_categories lc on lc.logcat_id = ic.logcat_id
-left join /*+jtype(h),distrib(l,a)*/ DMA.current_microcategories cm on cm.microcat_id = t.microcat_id
+left join DMA.current_microcategories cm on cm.microcat_id = t.microcat_id
 hierarchy (
     (vertical_id, logical_category_id, logical_param1_id, logical_param2_id),
     (category_id, subcategory_id, param1_id, param2_id, param3_id, param4_id)
-);
+)
+primary_key (infmquery_id, microcat_id);
---
+++ dimetra/enrichments/user_segment_market.sql
@@ -0,0 +1,18 @@
+create enrichment user_segment_market as select
+first(us.user_segment_market, ls.segment) as user_segment_market
+from :fact_table t
+left join (
+  select user_id,
+         converting_date as first_date,
+         lead(converting_date, 1, '2030-12-31') over(partition by user_id, logical_category_id order by converting_date) as last_date,
+         user_segment as user_segment_market,
+         logical_category_id
+  from dma.user_segment_market us
+  where user_id in (select user_id from :fact_table where event_date = '2022-01-01')
+) us
+  on us.user_id = t.user_id
+  and us.logical_category_id = t.logical_category_id
+  and t.__date__ between us.first_date and us.last_date
+
+LEFT JOIN dict.segmentation_ranks ls on ls.logical_category_id = t.logcat_id and ls.is_default
+primary_key(user_id, logical_category_id, logcat_id, __date__)
