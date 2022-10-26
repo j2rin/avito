739fff9d43ba03988d09f45527057ff71579891d
dakhaytin <dakhaytin@avito.ru>
2022-10-26 17:21:20 +0300

    OLAP-14420 fix typo

--- dimetra/enrichments/microcat_tree.sql
+++ dimetra/enrichments/microcat_tree.sql
@@ -1,7 +1,7 @@
 create enrichment microcat_tree as
 select
-    first(lc.vertical_id, cm.vertical_id)                              as vertical_id,
-    first(lc.logical_category_id, cm.logical_category_id)              as logical_category_id,
+    first(lc.vertical_id, cm.vertical_id, cm_legacy.subcategory_id)                              as vertical_id,
+    first(lc.logical_category_id, cm.logical_category_id, cm_legacy.subcategory_id)              as logical_category_id,
     lc.logical_param1_id,
     lc.logical_param2_id,
     lc.logcat_id,
