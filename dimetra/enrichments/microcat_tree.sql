73cfcfb68fc63f209c6e3df30f711a5847f9353e
dakhaytin <dakhaytin@avito.ru>
2022-11-08 15:16:17 +0300

    OLAP-14420 fix typo

--- dimetra/enrichments/microcat_tree.sql
+++ dimetra/enrichments/microcat_tree.sql
@@ -10,12 +10,12 @@ select
     first(cm.Param1_microcat_id, cm_legacy.Param1_microcat_id)         as param1_id,
     first(cm.Param2_microcat_id, cm_legacy.Param2_microcat_id)         as param2_id,
     first(cm.Param3_microcat_id, cm_legacy.Param3_microcat_id)         as param3_id,
-    first(cm.Param4_microcat_id, cm_legacy.Param4_microcat_id)         as param4_id,
+    first(cm.Param4_microcat_id, cm_legacy.Param4_microcat_id)         as param4_id
 from :fact_table  t
 left join infomodel.current_infmquery_category ic on ic.infmquery_id = t.infmquery_id
 left join dma.current_logical_categories lc on lc.logcat_id = ic.logcat_id
+left join DMA.current_microcategories cm_legacy on cm_legacy.microcat_id = ic.microcat_id
 left join DMA.current_microcategories cm on cm.microcat_id = t.microcat_id
-left join DMA.current_microcategories cm_legacy on cm.microcat_id = ic.microcat_id
 hierarchy (
     (vertical_id, logical_category_id, logical_param1_id, logical_param2_id),
     (category_id, subcategory_id, param1_id, param2_id, param3_id, param4_id)
