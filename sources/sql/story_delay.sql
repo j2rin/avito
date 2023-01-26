2bed7fecbc604944d1395c4ac1eb1268fcdca922
Sergey Timofeev <satimofeev@avito.ru>
2023-01-26 15:01:37 +0300

    BX-2022-1

--- sources/sql/story_delay.sql
+++ sources/sql/story_delay.sql
@@ -1,4 +1,4 @@
-with /*+ENABLE_WITH_CLAUSE_MATERIALIZATION */
+with
 tmp as (select
     platform_id, event_date, cookie_id,
     TIMESTAMPDIFF('ms',
@@ -8,6 +8,7 @@ tmp as (select
     max(case when eid=4203 then 1 end) as eid_4203,
     max(case when eid=4233 then 1 end) as eid_4233
     from dma.story_delay
+    where event_date::date between :first_date and :last_date
     group by 1,2,3)
 select platform_id, event_date, cookie_id,
 case when delay >= 0 and delay <= 10000 then delay end as delay,
@@ -15,6 +16,4 @@ case when delay >= 0 and delay <= 10000 then delay end as delay,
 (eid_4203 is null and eid_4233 is not null)::int as only_click,
 (eid_4203 is not null and eid_4233 is null)::int as only_show
 from tmp
-where csc.cookie_id is not null
-    and event_date::date between :first_date and :last_date
 
