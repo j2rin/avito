068199064943f3c20c5b38a80371cab2da2a9ae5
Sergey Timofeev <satimofeev@avito.ru>
2023-01-26 16:25:24 +0300

    BX-2022-1

--- sources/sql/story_delay.sql
+++ sources/sql/story_delay.sql
@@ -1,5 +1,9 @@
-with
-tmp as (select
+select platform_id, event_date, cookie_id,
+    case when delay >= 0 and delay <= 10000 then delay end as delay,
+    (eid_4203 is not null and eid_4233 is not null)::int as both_event,
+    (eid_4203 is null and eid_4233 is not null)::int as only_click,
+    (eid_4203 is not null and eid_4233 is null)::int as only_show
+from (select
     platform_id, event_date, cookie_id,
     TIMESTAMPDIFF('ms',
         max(case when eid=4203 then event_time end),
@@ -9,11 +13,6 @@ tmp as (select
     max(case when eid=4233 then 1 end) as eid_4233
     from dma.story_delay
     where event_date::date between :first_date and :last_date
-    group by 1,2,3)
-select platform_id, event_date, cookie_id,
-case when delay >= 0 and delay <= 10000 then delay end as delay,
-(eid_4203 is not null and eid_4233 is not null)::int as both_event,
-(eid_4203 is null and eid_4233 is not null)::int as only_click,
-(eid_4203 is not null and eid_4233 is null)::int as only_show
-from tmp
+    group by 1,2,3
+    )_
 
