061aa84e84563240ab0d8a9ef78566c238d54831
Danila Lenkov <dlenkov@avito.ru>
2023-02-16 16:22:43 +0300

    AB-1887 perf

---
+++ sources/sql/perf_queried_hosts_durations.sql
@@ -0,0 +1,12 @@
+select
+    track_id,
+    event_no,
+    event_date,
+    cookie_id,
+    user_id,
+    platform_id,
+    host,
+    events_count,
+    duration_sum
+from dma.perf_queried_hosts_durations d
+where event_date::date between :first_date and :last_date
