35ff5a2a1052817e875b983c3d973cf642b27ad7
Danila Lenkov <dlenkov@avito.ru>
2023-02-16 14:20:54 +0300

    AB-1887 perf.sql

---
+++ sources/sql/perf_web.sql
@@ -0,0 +1,20 @@
+select
+    observation_date,
+    cookie_id,
+    user_id,
+    platform_id,
+    platform_name,
+    network_type,
+    screen_name,
+    stage_name,
+    events,
+    events_p25,
+    events_p50,
+    events_p75,
+    events_p95,
+    events_sla,
+    duration,
+    duration_events,
+    initial_page_render
+from dma.o_perf_web w
+where observation_date::date between :first_date and :last_date
