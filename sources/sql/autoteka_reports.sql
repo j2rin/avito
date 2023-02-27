25e463ee48367c424207fc6cac8510b9835fe306
Artur Verdyan <agverdyan@avito.ru>
2023-02-27 17:35:29 +0300

    ROAD-14719

--- sources/sql/autoteka_reports.sql
+++ sources/sql/autoteka_reports.sql
@@ -1,6 +1,6 @@
 with /*+ENABLE_WITH_CLAUSE_MATERIALIZATION */
 items as (
-    select distinct hi.item_id
+    select distinct hi.item_id, hi.External_ID as item_ext_id
     from dma.autoteka_report_attributes ar
     join dma_autoteka.report_details rd on rd.report_id = ar.report_id
     join /*+distrib(a,l)*/ dds.H_Item hi on hi.External_ID = rd.avito_item_id
@@ -8,38 +8,42 @@ items as (
       and autoteka_order_id is not null
 ),
 orders as (
-    select distinct ar.autoteka_order_id
+    select distinct ar.autoteka_order_id, report_id
     from dma.autoteka_report_attributes ar
     where autoteka_package_history_created_at::date between :first_date and :last_date
       and autoteka_order_id is not null
 ),
+report_details as (
+    select rd.report_id,
+           rd.vin,
+           rd.mileage,
+           rd.year,
+           rd.avito_item_id
+    from dma_autoteka.report_details rd
+    where report_id in (select distinct coalesce(report_id, 0) from orders)
+),
+stream as (
+    select
+        autotekaorder_id,
+        autotekauser_id,
+        autoteka_cookie_id,
+        user_id,
+        cookie_id,
+        is_pro,
+        searchtype,
+        is_new_user,
+        platform_id,
+        amount,
+        reports_count,
+        autoteka_platform_id
+    from dma.autoteka_stream
+    where funnel_stage_id = 4
+      and autotekaorder_id in (select distinct coalesce(autoteka_order_id, 0) from orders)
+      and event_date::date between (:first_date - 180) and :last_date
+),
 item_price as (select item_id, actual_date, price from dds.S_Item_Price where item_id in (select item_id from items) and actual_date::date <= :last_date),
 item_location as (select item_id, actual_date, location_id from dds.L_Item_Location where item_id in (select item_id from items) and actual_date::date <= :last_date),
-item_microcat as (select item_id, actual_date, microcat_id from dds.L_Item_MicroCat where item_id in (select item_id from items) and actual_date::date <= :last_date),
-items_hist as (
-    select
-        hi.item_ext_id,
-        hi.item_id,
-        hi.autoteka_package_history_created_at,
-        p.price,
-        l.location_id,
-        m.microcat_id
-    from (
-        select
-            hi.item_id,
-            ar.autoteka_package_history_created_at,
-            max(rd.avito_item_id) as item_ext_id
-        from dma.autoteka_report_attributes ar
-        join dma_autoteka.report_details rd on rd.report_id = ar.report_id
-        join /*+distrib(a,l)*/ dds.H_Item hi on hi.External_ID = rd.avito_item_id
-        where autoteka_package_history_created_at::date between :first_date and :last_date
-        group by 1, 2
-        order by item_id, autoteka_package_history_created_at
-    ) hi
-    left join item_price p on p.Item_id = hi.Item_id and hi.autoteka_package_history_created_at interpolate previous value p.Actual_date
-    left join item_location l on l.Item_id = hi.Item_id  and hi.autoteka_package_history_created_at  interpolate previous value l.Actual_date
-    left join item_microcat m on m.Item_id = hi.Item_id  and hi.autoteka_package_history_created_at  interpolate previous value m.Actual_date
-)
+item_microcat as (select item_id, actual_date, microcat_id from dds.L_Item_MicroCat where item_id in (select item_id from items) and actual_date::date <= :last_date)
 select
     ar.autoteka_package_history_id,
     ar.autoteka_package_history_created_at::date,
@@ -69,9 +73,9 @@ select
        else 'Undefined'
     end as 'autoteka_car_age_group',
     case
-       when ih.Price <= 500000 then '0-500K'
-       when ih.Price > 500000 and  ih.Price <= 1000000 then '500K-1M'
-       when ih.Price > 1000000 then '1M+'
+       when p.Price <= 500000 then '0-500K'
+       when p.Price > 500000 and  p.Price <= 1000000 then '500K-1M'
+       when p.Price > 1000000 then '1M+'
        else 'Undefined'
     end as 'autoteka_car_price_group',
     case
@@ -84,7 +88,7 @@ from (
     select
         autoteka_package_history_id,
         ara.autoteka_package_history_created_at,
-        aus.autotekauser_id as autoteka_user_id,
+        cas.autotekauser_id as autoteka_user_id,
         cas.user_id,
         cas.cookie_id,
         cas.autoteka_cookie_id,
@@ -103,40 +107,7 @@ from (
         ars.banner_clicks,
         amount / reports_count as revenue_reports_used
     from dma.autoteka_report_attributes ara
-    join dma.autoteka_user_reports aus on aus.AutotekaUserReport_id = ara.autoteka_user_report_id
-    join (
-        select
-            autotekaorder_id,
-            autotekauser_id,
-            autoteka_cookie_id,
-            user_id,
-            cookie_id,
-            is_pro,
-            searchtype,
-            is_new_user,
-            platform_id,
-            amount,
-            reports_count,
-            autoteka_platform_id
-        from (
-            select
-                autotekaorder_id,
-                autotekauser_id,
-                autoteka_cookie_id,
-                user_id,
-                cookie_id,
-                is_pro,
-                searchtype,
-                is_new_user,
-                platform_id,
-                amount,
-                reports_count,
-                autoteka_platform_id
-            from dma.autoteka_stream
-            where funnel_stage_id = 4
-          	  and autotekaorder_id in (select autoteka_order_id from orders)
-        ) _
-    ) cas on cas.autotekaorder_id = ara.autoteka_order_id
+    join stream cas on cas.autotekaorder_id = ara.autoteka_order_id
     left join (
          select
             autoteka_user_report_id,
@@ -146,9 +117,12 @@ from (
             and event_date::date between :first_date and :last_date
          group by 1
     ) ars on ars.autoteka_user_report_id = ara.autoteka_user_report_id
-    left join dma_autoteka.report_details rd on rd.report_id = ara.report_id
+    left join report_details rd on rd.report_id = ara.report_id
     where autoteka_package_history_created_at::date between :first_date and :last_date
 ) ar
-left join items_hist ih on ih.item_ext_id = ar.item_ext_id and ih.autoteka_package_history_created_at = ar.autoteka_package_history_created_at
-left join dma.current_microcategories mc on  mc.microcat_id = ih.Microcat_id and mc.vertical='Transport'
-left join dma.current_locations cl on cl.Location_id = ih.Location_id
+left join items ih on ih.item_ext_id = ar.item_ext_id
+left join item_price p on p.Item_id = ih.Item_id and ar.autoteka_package_history_created_at interpolate previous value p.Actual_date
+left join item_location l on l.Item_id = ih.Item_id  and ar.autoteka_package_history_created_at  interpolate previous value l.Actual_date
+left join item_microcat m on m.Item_id = ih.Item_id  and ar.autoteka_package_history_created_at  interpolate previous value m.Actual_date
+left join dma.current_microcategories mc on  mc.microcat_id = m.Microcat_id and mc.vertical='Transport'
+left join dma.current_locations cl on cl.Location_id = l.Location_id;
