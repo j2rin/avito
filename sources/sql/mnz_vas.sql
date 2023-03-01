1e9ecf9b7a68c3b909adf6acb894e8d973d6e254
Danila Lenkov <dlenkov@avito.ru>
2023-03-01 16:22:01 +0300

    AB-1887 optimize mnz_vas

--- sources/sql/mnz_vas.sql
+++ sources/sql/mnz_vas.sql
@@ -1,15 +1,9 @@
-with am_client_day as (
-select user_id,
-       active_from_date,
-       active_to_date,
-       (personal_manager_team is not null and user_is_asd_recognised) as is_asd,
-       user_group_id
-from DMA.am_client_day_versioned
-),
-usm as (
-    select user_id, logical_category_id, user_segment, converting_date,
-        lead(converting_date, 1, '20990101') over(partition by user_id, logical_category_id order by converting_date) as next_converting_date
-    from DMA.user_segment_market
+with /*+ENABLE_WITH_CLAUSE_MATERIALIZATION */
+users as (
+    select distinct user_id
+    from dma.mnz_vas_seller_metrics
+    where event_date::date between :first_date and :last_date
+        and user_id is not null
 )
 select
 	mv.event_date,
@@ -32,11 +26,6 @@ select
 	amount_net,
 	transaction_counter,
 	page_from,
-		case
-		when acd.is_asd then 'ASD'
-		when mv.event_date >= us.converting_date then 'Pro'
-		else 'Private' end as
-	user_segment,
 	cm.Param1_microcat_id                                        as param1_id,
 	cm.Param2_microcat_id                                        as param2_id,
 	cm.Param3_microcat_id                                        as param3_id,
@@ -67,9 +56,22 @@ left join /*+jtype(h),distrib(l,a)*/ (
 left join  dma.current_logical_categories lc on lc.logcat_id = ic.logcat_id
 left join /*+jtype(h),distrib(l,b)*/ dict.segmentation_ranks ls on ls.logical_category_id = lc.logical_category_id and ls.is_default
 left join /*+jtype(h),distrib(l,a)*/ DMA.current_locations cl on cl.Location_id = mv.location_id
-left join /*+jtype(h),distrib(l,a)*/ DMA.user_segments us on mv.user_id = us.user_id and lc.logical_category_id = us.logical_category_id
-left join /*+jtype(h),distrib(l,a)*/  usm on mv.user_id = usm.user_id
-										and lc.logical_category_id = usm.logical_category_id
-										and mv.event_date >= usm.converting_date and mv.event_date < usm.next_converting_date
-left join /*+jtype(h),distrib(l,a)*/ am_client_day acd on mv.user_id = acd.user_id and mv.event_date between acd.active_from_date and acd.active_to_date
+left join /*+jtype(h),distrib(l,a)*/  (
+    select user_id, logical_category_id, user_segment, converting_date,
+        lead(converting_date, 1, '20990101') over(partition by user_id, logical_category_id order by converting_date) as next_converting_date
+    from DMA.user_segment_market
+    where user_id in (select user_id from users)
+) usm
+    on mv.user_id = usm.user_id
+    and lc.logical_category_id = usm.logical_category_id
+    and mv.event_date >= usm.converting_date and mv.event_date < usm.next_converting_date
+left join /*+jtype(h),distrib(l,a)*/ (
+    select user_id,
+           active_from_date,
+           active_to_date,
+           (personal_manager_team is not null and user_is_asd_recognised) as is_asd,
+           user_group_id
+    from DMA.am_client_day_versioned
+    where user_id in (select user_id from users)
+) acd on mv.user_id = acd.user_id and mv.event_date between acd.active_from_date and acd.active_to_date
 where mv.event_date::date between :first_date and :last_date
