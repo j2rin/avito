73d2d5d63be4932b84b728cd9e6a3d3125134ba7
Lyubov Levchenko <lllevchenko@avito.ru>
2023-03-21 16:20:09 +0300

    BBC-4115 Splitted ads revenue in dma.other_projects_revenue => fixed congifs

--- dimetra/metrics_do_not_touch/other_projects_revenue.sql
+++ dimetra/metrics_do_not_touch/other_projects_revenue.sql
@@ -7,9 +7,9 @@ from dma.vo_other_projects_revenue t
 
 create metrics other_projects_revenue as
 select
-    sum(case when project_type in ('advertisement_direct', 'advertisement_indirect')  then amount_net_adj end) as advertisement_amount_net_adj,
+    sum(case when project_type = 'advertisement' then amount_net_adj end) as advertisement_amount_net_adj,
     sum(case when project_type = 'autoteka' then amount_net_adj end) as autoteka_amount_net_adj,
-    sum(case when project_type in ('advertisement_direct_increased_for_сpm', 'advertisement_indirect_increased_for_сpm') then amount_net_adj end) as cnt_advertisement_amount_net_adj_increased_for_rpm,
+    sum(case when project_type = 'advertisement_increased_for_rpm' then amount_net_adj end) as cnt_advertisement_amount_net_adj_increased_for_rpm,
     sum(case when project_type = 'domofond' then amount_net_adj end) as domofond_amount_net_adj
 from other_projects_revenue t
 ;
