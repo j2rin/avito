0b09acf02efeaf6bf2a7c688ef3866b5f3e3f3c5
Georgiy Fandeev <gvfandeev@avito.ru>
2023-04-14 22:34:49 +0300

    BX-21558

--- sources/sql/kmt_stream.sql
+++ sources/sql/kmt_stream.sql
@@ -29,4 +29,5 @@ select
     cm.Param3_microcat_id   as param3_id,
     cm.Param4_microcat_id   as param4_id
 from dma.kmt_stream t 
-left join /*+jtype(h),distrib(l,a)*/ DMA.current_microcategories cm on cm.microcat_id = t.microcat_id;
+left join /*+jtype(h),distrib(l,a)*/ DMA.current_microcategories cm on cm.microcat_id = t.microcat_id
+where event_date::date between :first_date and :last_date;
