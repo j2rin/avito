44d602a81a0367f08c6203e58f4a2a560ed54ba4
Sergey Timofeev <satimofeev@avito.ru>
2023-06-19 20:14:48 +0300

    BX-14584

--- sources/sql/ux_feedback.sql
+++ sources/sql/ux_feedback.sql
@@ -1,8 +1,8 @@
 select
 user_id,
 created_at::date event_date,
-avg(decode(answer,'Отлично',5,'Хорошо',4,'Нейтрально',3,'Плохо',2,'Ужасно',1)) grade,
-sum(1) obs_value
+max(decode(answer,'Отлично',5,'Хорошо',4,'Нейтрально',3,'Плохо',2,'Ужасно',1)) grade,
+sum(1) obs_count
 from EXTERNAL_DATA.uxfeedback_answers
 join dma.current_user on External_id = user_id_ext::!int
 where question_number = 1
