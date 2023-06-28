4fd812fc13c094e6c7466c88c721f50dac4e41c5
Sofya Priemysheva <sistepanova@avito.ru>
2023-06-28 12:58:05 +0200

    ASDA-5352: fast fix -1 values of vertical and logical category for new_pro_listers

--- sources/sql/new_pro_listers.sql
+++ sources/sql/new_pro_listers.sql
@@ -1,12 +1,12 @@
 select
     event_date,
     user_id,
-    vertical_id,
-    logical_category_id,
+    vertical_id=-1 as vertical_is_null,
+    logical_category_id=-1 as logical_category_is_null,
+    case when vertical_id!=-1 then vertical_id end as vertical_id,
+    case when logical_category_id!=-1 then logical_category_id end as logical_category_id,
     less_1_month,
     is_asd,
     asd_user_group_id
 from dma.new_pro_listers
 where event_date::date between :first_date and :last_date
-    and logical_category_id != -1
-    and vertical_id != -1
