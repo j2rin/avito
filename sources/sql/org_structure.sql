a1e6384df230aa324f580e71274f6c69606c5129
Artem Tertychnyy <aotertychnyy@avito.ru>
2023-07-26 22:13:12 +0300

    DQ-324

---
+++ sources/sql/org_structure.sql
@@ -0,0 +1,16 @@
+select
+    event_date,
+    hash(login) as user_hash,
+    login as ldap_user,
+    functional_line_name as functional_line,
+    major_function_name as org_major_function,
+    unit_id as org_unit_id,
+    cluster_id as org_cluster_id,
+    vertical_id as org_vertical_id,
+    date_of_entry,
+    date_of_dismissal
+from dma.org_structure_full s
+join dict.calendar c on c.event_date between s.date_of_entry and coalesce(s.date_of_dismissal, :last_date)
+where record_priority = 1
+and date_of_entry <= :last_date
+and (date_of_dismissal >= :first_date or date_of_dismissal is null)
