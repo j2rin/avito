70aa9162fb28b7ccd09bb01927485eac6e2421ac
Evgeniy Chumachenko <epchumachenko@avito.ru>
2023-08-01 17:32:23 +0300

    REC-8149: add col source to sql

--- sources/sql/sq_track_metrics.sql
+++ sources/sql/sq_track_metrics.sql
@@ -1,10 +1,11 @@
-select /*+syntactic_join*/
-	   event_date,
-	   cookie_id,
-	   item_id,
-	   delay,
-	   execution_date,
-	   metric_name,
-	   metric_value
+select
+	event_date,
+	cookie_id,
+	item_id,
+	delay,
+	execution_date,
+	source,
+	metric_name,
+	metric_value
 from DMA.sq_track_metrics
 where event_date::date between :first_date and :last_date
