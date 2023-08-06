5ba22b680d54a61cffa10331f4c0dd424e9cb89c
Marina Rassadina <mirassadina@avito.ru>
2023-08-06 16:05:38 +0300

    AF-2612 фикс sql источника item_abuses

--- sources/sql/item_abuses.sql
+++ sources/sql/item_abuses.sql
@@ -1,16 +1,15 @@
 select     abuse_date,
            iu.user_id,
            cat.Title as abuse_type,
-           p.external_id as platform,
+           platform_id,
            source,
-           cm.category_name as category,
-           cm.subcategory_name as subcategory,
-           cm.logical_category,
-           cm.vertical
+           cm.category_id,
+           cm.subcategory_id,
+           cm.logcat_id as logical_category_id,
+           cm.vertical_id
     from dma.current_abuses a
     join dds.l_item_user as iu
     using(item_id)
     join dma.v_current_AbuseType cat on cat.AbuseType_id = a.AbuseType_id
     join dma.current_microcategories cm on cm.microcat_id = a.microcat_id
-    left join dds.H_Platform p using(Platform_id)
 where abuse_date::date between :first_date and :last_date
