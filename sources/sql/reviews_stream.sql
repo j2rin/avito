45404d9b288bfe7b45cd5bb1017af9aef7e09211
Sofiya Kokanova <svkokanova@avito.ru>
2023-09-11 16:05:23 +0300

    RAR-7436 page_from

--- sources/sql/reviews_stream.sql
+++ sources/sql/reviews_stream.sql
@@ -6,8 +6,15 @@ select
     cookie_id,
     user_id,
     platform_id,
-    eid,
+    2754 as eid,
     reviews_sort,
-    profile_id
+    profile_id, 
+    case 
+        when eid = 6372 and page_from = 'item' then 'item_scroll' 
+        else page_from 
+    end as page_from
 from dma.reviews_stream
 where event_date::date between :first_date and :last_date
+    and not (eid = 2754 and nvl(page_from,'none') in ('item_rating','item_scroll','item_all_reviews'))
+    and not (eid = 2754 and nvl(page_from,'none') = 'item' and platform_id = 3)
+
