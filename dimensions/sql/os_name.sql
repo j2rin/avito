3549e45b495983fb78b5843c28492ab0aad80e07
Vladislav Bozhev <vabozhev@avito.ru>
2023-09-29 13:25:57 +0300

    AB-2504 dimensions sql optimized

--- dimensions/sql/os_name.sql
+++ dimensions/sql/os_name.sql
@@ -1,13 +1,4 @@
-select value::varchar(64)
-from (
-    select public.transpose(* using parameters dimentions='cnt_cookie') over ()
-    from (
-        select
-            app_version, vendor, type_device, os_version, os_name, model, browser, app_category, app, display_size, display_X, display_Y, browser_version, sum(cnt_cookie) as cnt_cookie
-        from dma.useragent_day
-        group by 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13
-    ) u
-) u
-where col = 'os_name'
-group by 1
+select os_name::varchar(64) as value
+from dma.useragent_day
+group by os_name
 having sum(cnt_cookie) > 1000
