30a899a925512e6a541fdd99658fe10ea044a020
Aleksey Afanasev <anafanasev@avito.ru>
2023-10-03 17:23:15 +0400

    SS-16134 remove legacy metrics

--- sources/sql/support_comment.sql
+++
@@ -1,6 +0,0 @@
-select comment_create_date,
-	   comment_id,
-       SatisfactionScore,
-       line
-from dma.comment_satisfaction_scores
-where cast(comment_create_date as date) between :first_date and :last_date
