a454c1f07e1eee5e2d4fa5c69447085a1a61693b
Artem Dronov <agdronov@avito.ru>
2023-10-04 15:34:44 +0300

    AB-2595

--- sources/sql/clickstream_data_platform.sql
+++ sources/sql/clickstream_data_platform.sql
@@ -12,7 +12,7 @@ select
     url,
     properties,
     case
-        when eid in (9248, 9249) maplookup(mapjsonextractor(properties), 'field')
+        when eid in (9248, 9249) then maplookup(mapjsonextractor(properties), 'field')
     else null end as as properties_field
 from dwhcs.clickstream_data_platform
 where date(dtm) between :first_date and :last_date
