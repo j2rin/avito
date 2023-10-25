b35946faccd2fa934235518e77f86a6d8db7b4ae
Armen Esayan <adesayan@avito.ru>
2023-10-25 18:45:53 +0300

    COMN-9145: add new field "notification"

--- sources/sql/notification.sql
+++ sources/sql/notification.sql
@@ -9,6 +9,7 @@ SELECT
     notification_type, 
     notification_value, 
     is_notifications_on, 
-    events_count
+    events_count,
+    notification
 FROM dma.notifications_metric_observation_new d
 where cast(observation_date as date) between :first_date and :last_date
