493dd3009ef748a386721baa0a42240c4c26278b
dstoporova <dstoporova@avito.ru>
2023-10-27 11:04:26 +0300

    ADV-6971

---
+++ sources/sql/profilepromo_funnel.sql
@@ -0,0 +1,23 @@
+select  /*+syntactic_join*/
+	track_id,
+	event_no,
+	event_date,
+	event_timestamp,
+	eid,
+	platform_id,
+	cookie_id,
+	f.user_id,
+	alid,
+	profile_id,
+	f.adv_campaign_id,
+    adv_creative_id,
+    s.loc_id as region_id,
+    cm.logical_category_id,
+    vertical_id,
+    banner_code,
+    item_id
+from dma.profilepromo_funnel f
+left join /*+jtype(h),distrib(l,a)*/ dma.profilepromo_campaigns c on c.adv_campaign_id = f.adv_campaign_id
+left join /*+jtype(h),distrib(l,a)*/ dma.profilepromo_presets s on c.preset_id = s.preset_id
+left join /*+jtype(h),distrib(l,a)*/ dma.current_microcategories cm on cm.logical_category = s.logical_category
+where cast(f.event_date as date) between :first_date and :last_date
