945d50f7cae33fa49e0dfda2d3b603f3a96021c8
Aleksandr Mirskoy <almirskoy@avito.ru>
2023-11-15 18:02:53 +0300

    SHOPXDISC-4198

--- sources/sql/delivery_orders.sql
+++ sources/sql/delivery_orders.sql
@@ -127,6 +127,5 @@ left join dma.current_microcategories cm on cm.microcat_id = co.microcat_id
 left join dma.current_locations as cl on co.warehouse_location_id = cl.location_id
 left join dma.current_locations as bl on co.buyer_location_id = bl.location_id
 where true 
-	and year(status_date) between year(:first_date) and year(:last_date)
     and date(co.status_date) between date(:first_date) and date(:last_date)
     -- and status_year between date_trunc('year', :first_date) and date_trunc('year', :last_date) -- @trino
