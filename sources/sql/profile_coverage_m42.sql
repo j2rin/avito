5b03fa0b77682c23f14d042a398c6a5f6d4258b9
Danil Ponomarev <doponomarev@avito.ru>
2023-11-17 18:21:22 +0300

    PS-6825 Added profile coverage metrics

--- sources/sql/profile_coverage_m42.sql
+++ sources/sql/profile_coverage_m42.sql
@@ -6,13 +6,13 @@ SELECT
     MAX(CASE WHEN vertical = 'General' THEN pro_users END) AS profile_coverage_abs_goods,
     MAX(CASE WHEN vertical = 'Job' THEN pro_users END) AS profile_coverage_abs_jobs,
     MAX(CASE WHEN vertical = 'Realty' THEN pro_users END) AS profile_coverage_abs_realty,
-    MAX(CASE WHEN vertical = 'Services' THEN pro_users END) AS profile_coveragex_abs_realty,
-    MAX(CASE WHEN vertical = 'Any' THEN (pro_users/base*10000)::int END) AS profile_coverage,
-    MAX(CASE WHEN vertical = 'Auto' THEN (pro_users/base*10000)::int END) AS profile_coverage_auto,
-    MAX(CASE WHEN vertical = 'General' THEN (pro_users/base*10000)::int END) AS profile_coverage_goods,
-    MAX(CASE WHEN vertical = 'Job' THEN (pro_users/base*10000)::int END) AS profile_coverage_jobs,
-    MAX(CASE WHEN vertical = 'Realty' THEN (pro_users/base*10000)::int END) AS profile_coverage_realty,
-    MAX(CASE WHEN vertical = 'Services' THEN (pro_users/base*10000)::int END) AS profile_coverage_services
+    MAX(CASE WHEN vertical = 'Services' THEN pro_users END) AS profile_coverage_abs_services,
+    MAX(CASE WHEN vertical = 'Any' THEN cast(pro_users/base*10000 as int) END) AS profile_coverage,
+    MAX(CASE WHEN vertical = 'Auto' THEN cast(pro_users/base*10000 as int) END) AS profile_coverage_auto,
+    MAX(CASE WHEN vertical = 'General' THEN cast(pro_users/base*10000 as int) END) AS profile_coverage_goods,
+    MAX(CASE WHEN vertical = 'Job' THEN cast(pro_users/base*10000 as int) END) AS profile_coverage_jobs,
+    MAX(CASE WHEN vertical = 'Realty' THEN cast(pro_users/base*10000 as int) END) AS profile_coverage_realty,
+    MAX(CASE WHEN vertical = 'Services' THEN cast(pro_users/base*10000 as int) END) AS profile_coverage_services
 FROM dma.profile_vertical_coverage
 WHERE event_date BETWEEN :first_date AND :last_date
-GROUP BY 1,2
+GROUP BY launch_id, event_date
