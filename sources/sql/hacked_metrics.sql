f865264327b995e7806dc7638008e7584ac67a5d
Alisa Shabalina <aashabalina@avito.ru>
2023-11-30 17:53:10 +0300

    AVITOID-10796 change source file and some mistakes fixed

--- sources/sql/hacked_metrics.sql
+++ sources/sql/hacked_metrics.sql
@@ -24,5 +24,16 @@ SELECT
     ,not_susp_auth_success             
     ,susp_auth_success_with_check      
     ,not_susp_auth_success_with_check 
+    ,is_hack_restore_phone             
+    ,is_hack_direct_auth               
+    ,is_hack_restore_email             
+    ,auth_attempts                     
+    ,auth_attempts_with_check          
+    ,auth_success                      
+    ,auth_success_with_check           
+    ,auth_attempts_31_days             
+    ,auth_attempts_with_check_31_days     
+    ,auth_success_31_days              
+    auth_success_with_check_31_days
 FROM dma.hacked_metrics 
 WHERE event_date BETWEEN :first_date AND :last_date
