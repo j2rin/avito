8c7649ce702b199c09e0f535e72a938320acd1d8
Nikita Kostromin <nskostromin@avito.ru>
2023-12-04 17:28:50 +0300

    AAUTO-6233

--- sources/sql/clickstream_c2c_counters.sql
+++ sources/sql/clickstream_c2c_counters.sql
@@ -20,7 +20,8 @@ left join DMA.current_item ci on ci.item_id = cs.item_id
 left join dma.current_locations cl on cl.Location_id = ci.location_id
 left join dds.H_Platform hp on hp.Platform_id = cs.business_platform
 where
-    eid in (9651, 4498, 9651, 9752, 9632)
+    eid in (9651, 4498, 4496, 9752, 9632)
     and event_date::date >= '2023-11-01'
     and event_date::date between :first_date and :last_date
-    and from_page in ('c2c_item_card', 'c2c_usp_banner', 'c2c_tinkoff_flow')
+    and from_page in ('c2c_item_card', 'c2c_usp_banner', 'c2c_tinkoff_flow', 'landing_tinkoff',
+     'tinkoff', 'autobrokerSber')
