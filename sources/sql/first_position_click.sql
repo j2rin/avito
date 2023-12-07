bf3125b927582ee5536a998a9d2b911ea366b9c6
Galina Yudnikova <gabosinzon@avito.ru>
2023-12-07 14:19:21 +0300

    OLAP-18596: first_position_click.sql edited online with Bitbucket

--- sources/sql/first_position_click.sql
+++ sources/sql/first_position_click.sql
@@ -12,3 +12,4 @@ select
     map10
 from dma.first_position_click
 where cast(event_date as date) between :first_date and :last_date
+--and event_month between date_trunc('month', :first_date) and date_trunc('month', :last_date) -- @trino
