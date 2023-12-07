7a8cdb22fa1b66bc8965533c4ee8d691fa7a2a16
Dmitriy Reyman <ddpimkin@avito.ru>
2023-12-07 02:02:22 +0100

    OLAP-18595. trino compatible sql

--- sources/sql/sq_track_metrics.sql
+++ sources/sql/sq_track_metrics.sql
@@ -9,4 +9,4 @@ select
 	metric_value
 from DMA.sq_track_metrics
 where cast(event_date as date) between :first_date and :last_date
-    -- and execution_month between date_trunc('month', :first_date) and date_trunc('month', :last_date) -- @trino
+--  and execution_month between date_trunc('month', :first_date) and date_trunc('month', :last_date) -- @trino
