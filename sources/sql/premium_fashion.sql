493a5ecbe0271d66cc804d25436fcf2c20d7b223
Maksim Novikov <monovikov@avito.ru>
2023-12-07 16:56:08 +0300

    OLAP-18596: trino compatible sql

--- sources/sql/premium_fashion.sql
+++ sources/sql/premium_fashion.sql
@@ -1,4 +1,5 @@
 select *
 from DMA.premium_events_tracker
 where 1=1
-    and event_date::date between :first_date and :last_date
+    and cast(event_date as date) between :first_date and :last_date
+    --and event_year between date_trunc('year', :first_date) and date_trunc('year', :last_date) -- @trino
