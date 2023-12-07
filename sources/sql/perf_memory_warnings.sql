85466654d700cc377c4b5b1e820b9fbc625bea43
Anastasiya Verkhovtseva <agverkhovtseva@avito.ru>
2023-12-07 13:57:38 +0300

    OLAP-18596: revert perf_memory_warnings

--- sources/sql/perf_memory_warnings.sql
+++ sources/sql/perf_memory_warnings.sql
@@ -8,4 +8,3 @@ select
     platform_id
 from dma.o_perf_memory_warnings w
 where cast(event_date as date) between :first_date and :last_date
--- and event_year between date_trunc('year', :first_date) and date_trunc('year', :last_date) -- @trino
