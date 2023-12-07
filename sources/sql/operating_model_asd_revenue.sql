a9151e5882d4968b2b3a561d3865f878d129ee2c
Nikita Dontsov <nadontsov@avito.ru>
2023-12-07 14:48:49 +0300

    OLAP-18596 operating_model_asd_revenue

--- sources/sql/operating_model_asd_revenue.sql
+++ sources/sql/operating_model_asd_revenue.sql
@@ -4,7 +4,7 @@ select
     npsm.vertical_id,
     npsm.logical_category_id,
     npsm.user_segment_market_paying as user_segment_market,
-    DATEDIFF('day', npsm.pro_paying_converted_date, pur.event_date) as days_after_pro_paying_convert,
+    DATE_DIFF('day', npsm.pro_paying_converted_date, pur.event_date) as days_after_pro_paying_convert,
     coalesce(cm.vertical_id = npsm.vertical_id, False) as is_payment_in_converted_vertical,
     ctt.transaction_type,
     ctt.transaction_subtype,
@@ -22,3 +22,4 @@ where cast(event_date as date) between :first_date and :last_date
     and npsm.pro_paying_converted_date is not null
     and npsm.is_new_pro_seller_total
     and ctt.IsRevenue
+    -- and event_year between date_trunc('year', :first_date) and date_trunc('year', :last_date) -- @trino
