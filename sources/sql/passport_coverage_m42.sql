1da4047e1dc7cba4de81eb919cd44a348b32f1b7
Maksim Vinogradov <mvvinogradov@avito.ru>
2023-12-07 16:45:22 +0300

    OLAP-18595: trino compatible sql - passport_coverage_m42

--- sources/sql/passport_coverage_m42.sql
+++ sources/sql/passport_coverage_m42.sql
@@ -4,3 +4,4 @@ SELECT
     weighted_coverage_multi * 10000 as weighted_coverage_multi
 FROM dma.passport_coverage
 WHERE calc_date BETWEEN :first_date AND :last_date
+--AND calc_year between date_trunc('year', :first_date) AND date_trunc('year', :last_date) --@trino;
