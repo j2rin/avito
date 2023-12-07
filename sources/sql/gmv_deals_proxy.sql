29088e793e6ae17c5c1b89a851e28af9467cd11e
Ilya Pavloskiy <ipavlovskiy@avito.ru>
2023-12-07 14:02:50 +0300

    OLAP-18596: gmv_deals_proxy

--- sources/sql/gmv_deals_proxy.sql
+++ sources/sql/gmv_deals_proxy.sql
@@ -1,25 +1,25 @@
 select
-     dp.event_date
-    ,dp.user_id
-    ,lc.vertical_id
-    ,lc.logical_category_id
-    ,case cl.level when 3 then cl.ParentLocation_id else cl.Location_id end as region_id
-    ,case cl.level when 3 then cl.Location_id end                           as city_id
-	,cm.category_id
-	,cm.subcategory_id
-	,coalesce(acd.is_asd, False) as is_asd
-    ,acd.user_group_id      as asd_user_group_id
-    ,dp.user_segment_market
-    ,dp.proxy_deals as proxy_deals
-    ,dp.gmv_volume as gmv_volume
-    ,dp.item_id
+    dp.event_date
+     ,dp.user_id
+     ,lc.vertical_id
+     ,lc.logical_category_id
+     ,case cl.level when 3 then cl.ParentLocation_id else cl.Location_id end as region_id
+     ,case cl.level when 3 then cl.Location_id end                           as city_id
+     ,cm.category_id
+     ,cm.subcategory_id
+     ,coalesce(acd.is_asd, False) as is_asd
+     ,acd.user_group_id      as asd_user_group_id
+     ,dp.user_segment_market
+     ,dp.proxy_deals as proxy_deals
+     ,dp.gmv_volume as gmv_volume
+     ,dp.item_id
 from dma.gmv_deals_proxy dp
-join dma.current_item ci on dp.item_id = ci.item_id
-left join dma.current_locations cl on ci.location_id = cl.location_id
-left join dma.current_logical_categories lc on dp.logical_category_id = lc.logcat_id
-left join dma.current_microcategories cm on dp.microcat_id = cm.microcat_id
+         join dma.current_item ci on dp.item_id = ci.item_id
+         left join dma.current_locations cl on ci.location_id = cl.location_id
+         left join dma.current_logical_categories lc on dp.logical_category_id = lc.logcat_id
+         left join dma.current_microcategories cm on dp.microcat_id = cm.microcat_id
 
-left join (
+         left join (
     select
         user_id,
         active_from_date,
@@ -28,9 +28,10 @@ left join (
         user_group_id
     from DMA.am_client_day_versioned
     where active_from_date <= :last_date
-        and active_to_date >= :first_date
+      and active_to_date >= :first_date
 ) acd
-    on   acd.user_id = dp.user_id
-    and  dp.event_date between acd.active_from_date and acd.active_to_date
+                   on   acd.user_id = dp.user_id
+                       and  dp.event_date between acd.active_from_date and acd.active_to_date
 
 where cast(event_date as date) between :first_date and :last_date
+  -- and event_year between date_trunc('year', :first_date) and date_trunc('year', :last_date) -- @trino
