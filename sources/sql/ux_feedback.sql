b0c491027bcb41b5bafac7b2a2f95fc2548f6db6
dvdremin <dvdremin@avito.ru>
2023-12-07 15:21:00 +0100

    OLAP-18596. trino compatible sql

--- sources/sql/ux_feedback.sql
+++ sources/sql/ux_feedback.sql
@@ -6,8 +6,8 @@ sum(1) obs_count,
 sum(case answer when 'Отлично' then 5 when 'Хорошо' then 4 when 'Нейтрально' then 3 when 'Плохо' then 2 when 'Ужасно' then 1 end) as obs_sum_grade
 from EXTERNAL_DATA.uxfeedback_answers
 join dma."current_user" on External_id =
-                                        -- user_id_ext::!int --@vertica
-                                        -- try_cast(user_id_ext as bigint) --@trino
+                                        -- user_id_ext::!int -- @vertica
+                                        -- try_cast(user_id_ext as bigint) -- @trino
 where question_number = 1
 and cast(created_at as date) between :first_date and :last_date
 group by 1,2
