487791dbb65496e0cd2ebd2f17d6138d05dfa012
Galina Yudnikova <gabosinzon@avito.ru>
2023-12-07 16:22:08 +0300

    OLAP-18596: time_to_interview.sql edited online with Bitbucket

--- sources/sql/time_to_interview.sql
+++ sources/sql/time_to_interview.sql
@@ -16,11 +16,11 @@ from (
             when cohort='interview' then platform_id_acceptance
             when cohort='activation' then platform_id_acceptance
             else platform_id_acceptance end as platform_id,
-        datediff('day', event_date_4066, event_date_invite) as time_to_invite_days,
-        datediff('day', event_date_4066, event_date_survey) as time_to_survey_days,
-        datediff('day', event_date_4066, chosen_interview_time) as time_to_interview,
-        datediff('day', item_activation_time, chosen_interview_time) as time_from_activation_to_interview,
-        datediff('day', event_date_4066, event_date_acceptance) as time_to_acceptance
+        date_diff('day', event_date_4066, event_date_invite) as time_to_invite_days,
+        date_diff('day', event_date_4066, event_date_survey) as time_to_survey_days,
+        date_diff('day', event_date_4066, chosen_interview_time) as time_to_interview,
+        date_diff('day', item_activation_time, chosen_interview_time) as time_from_activation_to_interview,
+        date_diff('day', event_date_4066, event_date_acceptance) as time_to_acceptance
     from dma.hiring_report dhr
     cross join
     (
@@ -34,5 +34,6 @@ from (
         union all
         select 'acceptance' as cohort
     )c
+--     where event_date_year between date_trunc('year', :first_date) and date_trunc('year', :last_date) -- @trino
 ) dhr
 where cast(event_time as date) between :first_date and :last_date
