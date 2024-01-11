166735d26da4f4a7dddfb64862683030e063d1c9
Ilya Komutkov <iskomutkov@avito.ru>
2024-01-11 22:16:04 +0800

    FMP-5317 improved source

--- sources/sql/installments_goods_events.sql
+++ sources/sql/installments_goods_events.sql
@@ -8,10 +8,11 @@ select
 from dma.installments_click_stream
 where eventtype_ext in (4496, 4497, 4498, 4801, 7820, 7826, 7889)
         or eventtype_ext=7817 and installment_screen = 'success_verification'
-        and event_date between :first_date and :last_date
-        
-union all 
-        
+        and event_date between cast(:first_date as date) and cast(:last_date as date)
+        -- and event_year between date_trunc('year', cast(:first_date as date) ) and date_trunc('year', cast(:last_date as date)) -- @trino
+
+union all
+
 select
         create_date as event_timestamp,
         cio.platform_id,
@@ -20,12 +21,12 @@ select
         item_id,
         'installment_order_creation' as event_name_slug
 from dma.current_installment_orders cio
-left join dds.h_platform hp 
+left join dds.h_platform hp
     on cio.platform_id = hp.platform_id
-where cast(create_date as date) between :first_date and :last_date
-    
-union all 
-        
+where cast(create_date as date) between cast(:first_date as date) and cast(:last_date as date)
+
+union all
+
 select
         pay_date as event_timestamp,
         cio.platform_id,
@@ -34,7 +35,7 @@ select
         item_id,
         'installment_order_payment' as event_name_slug
 from dma.current_installment_orders cio
-left join dds.h_platform hp 
+left join dds.h_platform hp
     on cio.platform_id = hp.platform_id
 where is_paid
-        and cast(pay_date as date) between :first_date and :last_date
+        and cast(create_date as date) between cast(:first_date as date) and cast(:last_date as date)
