0111d51f5e7e8c97d21f0dca9a466ade00b63bc5
Yana Zenkova <ypzenkova@avito.ru>
2024-01-17 17:34:24 +0300

    ALMP-904 add auction withdrawal metrics

---
+++ sources/sql/auction_withdrawal_refund.sql
@@ -0,0 +1,32 @@
+select
+    aw.u,
+    aw.cookie_id,
+    aw.transaction_uid,
+    aw.transaction_type,
+    aw.amount,
+    aw.event_time,
+    aw.item_id,
+    aw.campaign_id,
+    aw.x,
+    aw.reason,
+    aw.is_open_auction,
+    aw.event_date,
+    cm.vertical_id,
+    cm.logical_category_id,
+    cm.category_id,
+    cm.subcategory_id,
+    cl.region_internal_id as region_id
+from
+    dwhcs.auction_withdrawal_refund aw
+left join
+    dma.current_microcategories cm
+on
+    aw.microcat_id = cm.microcat_id
+    and is_active_microcat
+left join 
+    dma.current_locations cl
+on
+    aw.location_id = cl.location_id
+where
+    is_open_auction = 1 and
+    cast(event_date as date) between :first_date and :last_date
