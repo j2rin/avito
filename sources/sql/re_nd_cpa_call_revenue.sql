4380de9029379ee52d9e76591c531505caca4b23
Vladislav Kalino <vakalino@avito.ru>
2024-01-19 11:45:24 +0300

    ALMP-1138 adding metrics for cpa-calls revenue

--- sources/sql/re_nd_cpa_call_revenue.sql
+++ sources/sql/re_nd_cpa_call_revenue.sql
@@ -5,5 +5,7 @@ select
     call_source_detailed,
     cast(coalesce(sum(amount_net) / 1.2, 0) as int) as cpa_call_revenue_net_adj
 from dma.re_nd_cpa_call_source_detailed
-where cast(action_time as date) between :first_date and :last_date
+where
+    cast(action_time as date) between :first_date and :last_date
+    -- and action_year between :first_date and :last_date -- @trino
 group by 1, 2, 3, 4
