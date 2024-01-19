92f8f6ccdf8417c1654cd9eaaa3c7e467bcb942d
Vladislav Kalino <vakalino@avito.ru>
2024-01-19 10:55:45 +0300

    ALMP-1138 adding metrics for cpa-calls revenue

--- sources/sql/re_nd_cpa_call_revenue.sql
+++ sources/sql/re_nd_cpa_call_revenue.sql
@@ -2,9 +2,7 @@ select
     buyer_cookie_id as cookie_id,
     x_eid,
     cast(action_time as date) as event_date,
-    -- Dimensions -----------------------------------------------------------------------------------------------------
-    call_source_detailed, ---Очень желательно, чтобы у дименшена был айдишник, а не только varchar-представление.
-    -------------------------------------------------------------------------------------------------------------------
+    call_source_detailed,
     cast(coalesce(sum(amount_net) / 1.2, 0) as int) as cpa_call_revenue_net_adj
 from dma.re_nd_cpa_call_source_detailed
 where cast(action_time as date) between :first_date and :last_date
