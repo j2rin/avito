73e463a231a46f409c3e594372bb389e7d6b21a8
lvselivanova <lvselivanova@avito.ru>
2024-02-02 10:35:05 +0300

    [RAR-8155] Добавляю дополнительные разрезы и метрики

--- sources/sql/buyer_reviews_stream.sql
+++ sources/sql/buyer_reviews_stream.sql
@@ -1,12 +1,11 @@
+explain
 with orders as (
-    select deliveryorder_id,
-           count(distinct microcat_id) as count_microcats,
-           count(distinct location_id) as count_locations,
-           max(microcat_id) as microcat_id,
-           max(location_id) as location_id,
-           count(distinct microcat_id) > 1 or count(distinct location_id) > 1 as is_mixed_order
-    from dma.current_order_item
-    group by 1
+    select coi.deliveryorder_id,
+           Microcat_id,
+           Location_id
+    from dma.current_order_item coi
+    join dma.current_order co on coi.deliveryorder_id = co.deliveryorder_id
+    where items_qty = 1
     ),
     seller_has_review as (
     select from_user_id,
@@ -18,58 +17,23 @@ select data.*,
       -- сегмент селлера
       coalesce(usm.user_segment, sr.segment) as user_segment_market
 from (
-    select track_id, event_no, event_date, event_timestamp, cookie_id, user_id, platform_id, profile_id, review_page_from, review_add_trigger, eid, buyer_review_form_type, has_review, order_status, review_request_send_result,
-           is_mixed_order,
+    select track_id, event_no, event_date, event_timestamp, cookie_id, user_id, platform_id, profile_id, review_page_from, review_add_trigger, eid, buyer_review_form_type, has_review, order_status, review_request_send_result, internal_orderid,
     -- Dimensions -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     -- microcat_dimensions ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    case
-        when is_mixed_order then null -- смешанному заказу проставляю null
-        else vertical_id -- в других случаях просто забираю категорию айтема
-        end                                                                                          as vertical_id,
-    case
-        when is_mixed_order then null
-        else logical_category_id
-        end                                                                                          as logical_category_id,
-    case
-        when is_mixed_order then null
-        else category_id
-        end                                                                                          as category_id,
-    case
-        when is_mixed_order then null
-        else subcategory_id
-        end                                                                                          as subcategory_id,
-    case
-        when is_mixed_order then null
-        else param1_microcat_id
-        end                                                                                          as param1_id,
-    case
-        when is_mixed_order then null
-        else param2_microcat_id
-        end                                                                                          as param2_id,
-    case
-        when is_mixed_order then null
-        else param3_microcat_id
-        end                                                                                          as param3_id,
-    case
-        when is_mixed_order then null
-        else param4_microcat_id
-        end                                                                                          as param4_id,
+    vertical_id,
+    logical_category_id,
+    category_id,
+    subcategory_id,
+    Param1_microcat_id as param1_id,
+    Param2_microcat_id as param2_id,
+    Param3_microcat_id as param3_id,
+    Param4_microcat_id as param4_id,
     -- location_dimensions ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    case
-        when is_mixed_order then null
-        else case level when 3 then ParentLocation_id else cl.Location_id end end               as region_id,
-    case
-        when is_mixed_order then null
-        else case level when 3 then cl.Location_id end end                                      as city_id,
-    case
-        when is_mixed_order then null
-        else LocationGroup_id end                                                               as location_group_id,
-    case
-        when is_mixed_order then null
-        else City_Population_Group end                                                          as population_group,
-    case
-        when is_mixed_order then null
-        else Logical_Level end                                                                  as location_level_id
+    case level when 3 then ParentLocation_id else cl.Location_id end               as region_id,
+    case level when 3 then cl.Location_id end                                      as city_id,
+    LocationGroup_id                                                               as location_group_id,
+    City_Population_Group                                                          as population_group,
+    Logical_Level                                                                  as location_level_id
           from (
               select
                      track_id,
@@ -81,7 +45,7 @@ from (
                      business_platform                                                                                 as platform_id,
                      eid, buyer_id                                                                                     as profile_id,
                      review_request_send_result,
-                     is_mixed_order, -- признак goods, поэтому там, где он притянулся null'ом, его разбивка там просто не нужна
+                     internal_orderid,
                      coalesce (o.microcat_id, ci.microcat_id)                                                          as microcat_id,
                      coalesce (o.location_id, ci.location_id)                                                          as location_id,
                      coalesce(page_from, 'null')                                                                       as review_page_from,
@@ -96,7 +60,7 @@ from (
               left join dma.current_item ci on brs.item_id = ci.item_id and buyer_review_form_type != 'goods' -- забираю микрокат айтема не из goods (1 заказ = 1 айтем)
               left join seller_has_review cbr on brs.seller_id = from_user_id
               where cast(event_timestamp as date) between :first_date and :last_date
-              --and event_year between date_trunc('year', :first_date) and date_trunc('year', :last_date) -- @trino
+              and event_year between date_trunc('year', :first_date) and date_trunc('year', :last_date) -- @trino
               ) t1
               left join dma.current_microcategories cm on t1.microcat_id = cm.microcat_id
               left join dma.current_locations cl on t1.Location_id = cl.Location_id
