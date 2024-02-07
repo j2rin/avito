537ad520977ecb19cd8d2779c068b18d6246a957
Danila Lenkov <dlenkov@avito.ru>
2024-02-07 23:15:38 +0300

    OLAP-19078 mattermost_messages.sql

--- sources/sql/mattermost_messages.sql
+++ sources/sql/mattermost_messages.sql
@@ -3,8 +3,8 @@ select
     root_id is null as is_root,
     channel,
     user_name as ldap_user,
-    hash(user_name) as user_hash,
+    from_big_endian_64(xxhash64(user_name)) as user_hash,
     message,
-    regexp_ilike(message, '\b(слой|слоя|слою|слоем|слое|слои|слоев|слоям|слоями|слоях)\b') as is_subject_layer
+    regexp_like(lower(message), '\b(слой|слоя|слою|слоем|слое|слои|слоев|слоям|слоями|слоях)\b') as is_subject_layer
 from DMA.mattermost_messages
 where date(create_at) between date(:first_date) and date(:last_date)
