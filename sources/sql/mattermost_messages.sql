38d995aa36bf3182c5078bbb050f8ff39fbb5546
Danila Lenkov <dlenkov@avito.ru>
2024-02-08 13:56:09 +0300

    OLAP-19078 mattermost_messages.sql

--- sources/sql/mattermost_messages.sql
+++ sources/sql/mattermost_messages.sql
@@ -3,7 +3,7 @@ select
     root_id is null as is_root,
     channel,
     user_name as ldap_user,
-    from_big_endian_64(xxhash64(user_name)) as user_hash,
+    from_big_endian_64(xxhash64(cast(coalesce(user_name, '') as varbinary))) as user_hash,
     message,
     regexp_like(lower(message), '\b(слой|слоя|слою|слоем|слое|слои|слоев|слоям|слоями|слоях)\b') as is_subject_layer
 from DMA.mattermost_messages
