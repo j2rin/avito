383567e245aed335e0ce5c327e509ee1bd654f11
Danila Lenkov <dlenkov@avito.ru>
2024-02-08 14:06:34 +0300

    OLAP-19078 mattermost_messages.sql

--- sources/sql/mattermost_messages.sql
+++ sources/sql/mattermost_messages.sql
@@ -5,6 +5,6 @@ select
     user_name as ldap_user,
     from_big_endian_64(xxhash64(cast(coalesce(user_name, '') as varbinary))) as user_hash,
     message,
-    regexp_like(lower(message), '\b(слой|слоя|слою|слоем|слое|слои|слоев|слоям|слоями|слоях)\b') as is_subject_layer
+    regexp_like(lower(cast(message as varchar(32500))), '\b(слой|слоя|слою|слоем|слое|слои|слоев|слоям|слоями|слоях)\b') as is_subject_layer
 from DMA.mattermost_messages
 where date(create_at) between date(:first_date) and date(:last_date)
