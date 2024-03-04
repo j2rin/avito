cad2ad3d52cad954708d7e16cc59e9179e6206ab
Yuriy Rybakov <yvrybakov@avito.ru>
2024-03-04 16:02:44 +0300

    MRK-3319 true_new_users.sql edited online with Bitbucket

--- sources/sql/true_new_users.sql
+++ sources/sql/true_new_users.sql
@@ -43,4 +43,4 @@ left join /*+jtype(h),distrib(l,a)*/ dict.segmentation_ranks ls
     on   ls.logical_category_id = lc.logical_category_id
     and  ls.is_default
 where tnu.event_date between :first_date and :last_date
-    -- and event_year between date_trunc('year', date(:first_date)) and date_trunc('year', date(:last_date)) -- @trino
+-- and event_year between date_trunc('year', date(:first_date)) and date_trunc('year', date(:last_date)) -- @trino
