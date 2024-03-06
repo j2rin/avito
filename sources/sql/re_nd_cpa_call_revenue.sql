3be60f3f7f8517bf27df4416a0decd443fa4ac09
Vladislav Kalino <vakalino@avito.ru>
2024-03-06 23:10:46 +0200

    ALMP-1355 adding revenue metrics for CPA chats and CPA actions in NewDev

--- sources/sql/re_nd_cpa_call_revenue.sql
+++ sources/sql/re_nd_cpa_call_revenue.sql
@@ -49,6 +49,7 @@ with
         where
             c.is_paid
             and cast(c.event_date as date) between :first_date and :last_date
+            -- and event_year between date_trunc('year', :first_date) and date_trunc('year', :last_date) -- @trino
         ),
     events as
         (select
