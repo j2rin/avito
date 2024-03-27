a2a3570059b4e66a659e3e8443031f0cd9dea5ff
Gennadiy Ivanov <gaivanov@avito.ru>
2024-03-27 20:46:31 +0300

    BBC-4449_gmv_proxy
    id issues

--- sources/sql/proxy_deals_2_0.sql
+++ sources/sql/proxy_deals_2_0.sql
@@ -1,18 +1,12 @@
 select 
-    pd20.item_id
-    , pd20.buyer_id as user_id
-    , pd20.event_date
-    , clc.logical_category_id
-    , clc.vertical_id 
-    , pd20.prob_adj
-    , pd20.user_segment as user_segment_market
+    item_id
+    , buyer_id as user_id
+    , event_date
+    , logical_category_id
+    , vertical_id 
+    , prob_adj
+    , user_segment as user_segment_market
 from 
-    DMA.proxy_deals_2_0 as pd20
-left join DMA.current_item as ci
-    on pd20.item_id = ci.item_id
-left join infomodel.current_infmquery_category as cic
-    on ci.infmquery_id = cic.infmquery_id
-left join dma.current_logical_categories as clc
-    on cic.logcat_id = clc.logcat_id
-where cast(pd20.event_date as date) between cast(:first_date as date) and cast(:last_date as date)
+    DMA.proxy_deals_2_0 
+where cast(event_date as date) between cast(:first_date as date) and cast(:last_date as date)
 -- and event_year between date_trunc('year', cast(:first_date as date)) and date_trunc('year', cast(:last_date as date)) -- @trino
