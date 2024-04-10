82bd58411e5462047c5efc33d4ddb937e43183d8
Vladislav Bozhev <vabozhev@avito.ru>
2024-04-10 18:16:53 +0300

    AB-2817-common-sql-for-dims

--- dimensions/sql/buyer_review_form_type.sql
+++ dimensions/sql/buyer_review_form_type.sql
@@ -1,2 +1,3 @@
 select distinct coalesce(buyer_review_form_type, 'null') as value
 from dma.buyer_reviews_stream
+where event_year is not null
--- dimensions/sql/review_add_trigger.sql
+++ dimensions/sql/review_add_trigger.sql
@@ -1,2 +1,3 @@
 select distinct coalesce(review_add_trigger, 'null') as value
 from dma.buyer_reviews_stream
+where event_year is not null
