8bacfd6f0ae4b8732722db6e4ea6ccafd362ad0e
Aleksandra Sheveleva <agsheveleva@avito.ru>
2024-04-15 13:01:41 +0300

    BX-29009

--- sources/sql/short_video_upload_source.sql
+++ sources/sql/short_video_upload_source.sql
@@ -1,10 +1,10 @@
 select 
     video_upload_date event_date,
     upload_page.event_date video_try_date,
-    cs_chain.user_id,
-    cs_chain.cookie_id,
-    cs_chain.platform_id,
-    cs_chain.from_page,
+    upload_page.user_id,
+    upload_page.cookie_id,
+    upload_page.platform_id,
+    upload_page.from_page,
     video,
     coalesce(upload_page.item_id, cs_chain.item_id) item_id_upload_chain,
     item_video.item_id item_id,
@@ -12,11 +12,7 @@ select
     cm.vertical_id,
     cm.logical_category_id,
     cm.category_id,
-    cm.subcategory_id,
-    cm.Param1_microcat_id                                      as param1_id,
-    cm.Param2_microcat_id                                      as param2_id,
-    cm.Param3_microcat_id                                      as param3_id,
-    cm.Param4_microcat_id                                      as param4_id
+    cm.subcategory_id
 from 
 (
     select 
@@ -27,30 +23,37 @@ from
         from_page,
         event_chain,
         error_text,
-        cast(event_date as date) event_date
+        cast(event_date as date) event_date,
+        microcat_id
     from dma.clickstream_video
-    where event_date between cast(:first_date as date) - 2 and cast(:last_date as date) - 2
+    where event_date between cast(:first_date as date) - interval '2' day and cast(:last_date as date) - interval '2' day
         and eid = 6303
 ) upload_page
 left join 
 (   
-    select 
-        item_id,
-        event_chain
-    from dwhcs.clickstream_canon 
-    where event_date between cast(:first_date as date) - 2 and cast(:last_date as date) - 2
-        and eid = 3949
-) cs_chain using (event_chain)
+    select  
+        External_ID event_chain,
+        s.item_id,
+        s.microcat_id
+    from DMA.item_add_chain_metrics s
+        join dds.H_EventChain using (EventChain_id)
+    where cast(chain_item_create_time as date) between cast(:first_date as date) - interval '2' day and cast(:last_date as date) - interval '2' day
+) cs_chain on cs_chain.event_chain = upload_page.event_chain
 left join 
-(select * from
+(select 
+ 	item_id,
+ 	video,
+ 	actual_date video_upload_date
+ from
     (
         select 
             item_id,
             video,
-            cast(actual_date as date) video_upload_date
+            cast(actual_date as date) actual_date,
+      		row_number() over (partition by video order by actual_date) rn
             from dds.s_item_video
-            limit 1 over (partition by video order by actual_date)
     ) siv 
-    where siv.video_upload_date between cast(:first_date as date) - 2 and cast(:last_date as date) - 2
-) item_video on item_video.item_id = upload_page.item_id or item_video.item_id = cs_chain.item_id
-left join DMA.current_microcategories cm on cm.microcat_id = cs.microcat_id
+    where rn = 1 and siv.actual_date between cast(:first_date as date) - interval '2' day and cast(:last_date as date) - interval '2' day
+) item_video on item_video.item_id = coalesce(upload_page.item_id, cs_chain.item_id)
+left join DMA.current_microcategories cm on cm.microcat_id = coalesce(upload_page.microcat_id, cs_chain.microcat_id)
+
