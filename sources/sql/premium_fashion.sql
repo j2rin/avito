9cb755e6f0b838123c7673ef67b433442469176b
Anastasiya Anosova <aoanosova@avito.ru>
2024-04-23 20:04:36 +0300

    GNRL-34495

--- sources/sql/premium_fashion.sql
+++ sources/sql/premium_fashion.sql
@@ -1,6 +1,5 @@
 select * 
 from DMA.premium_events_tracker
-left join b2c_items bi on bi.item_id = pe.item_id
 where 1=1
     and cast(event_date as date) between :first_date and :last_date
     --and event_year between date_trunc('year', :first_date) and date_trunc('year', :last_date) -- @trino
