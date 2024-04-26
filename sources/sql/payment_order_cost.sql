22e0f12d4ceb4701ca611b717012306794e4c73d
aakishkurno <aakishkurno@avito.ru>
2024-04-26 16:09:37 +0200

    FMP-6769_tk

--- sources/sql/payment_order_cost.sql
+++ sources/sql/payment_order_cost.sql
@@ -13,6 +13,7 @@ with /*+ENABLE_WITH_CLAUSE_MATERIALIZATION */ delivery_status_date  as ( -- чт
 order_data as (
     select purchase_ext, min(platform_id) as platform_id ,min(buyer_id) as buyer_id, min(create_date) as create_date
     from dma.current_order
+    where cast(create_date as date) between :first_date and :last_date
     group by 1
 ),
 orders as (
@@ -22,12 +23,32 @@ orders as (
 buyers as (
     select distinct buyer_id
     from dma.current_order
-    where deliveryorder_id in (select deliveryorder_id from orders)
+    where deliveryorder_id in (select deliveryorder_id from orders))
+,ub as (
+    select /*+ SYNTACTIC_JOIN*/
+        co.purchase_id,
+        co.purchase_ext,
+        MAX(case when expired_date >= date('2022-03-01') and expired_date >= (create_date - interval '5' year) and create_date between status_start_at and coalesce(status_end_at, create_date) then True else False end) as has_avito_bindings,
+        SUM(case when expired_date >= date('2022-03-01') and expired_date >= (create_date - interval '5' year) and create_date between status_start_at and coalesce(status_end_at, create_date) then 1 else 0 end) as cnt_bindings
+    from dma.current_order co
+    left join /*+jtype(h)*/  dma.user_payment_bindings pb on pb.user_id = co.buyer_id
+
+    where true
+        and pb.external_source_provider_id in (18)
+        and pb.is_available = True
+               and co.deliveryorder_id in (select deliveryorder_id from orders)
+    group by 1,2
 ),
-sellers as (
-    select distinct seller_id
-    from dma.current_order_item
-    where deliveryorder_id in (select deliveryorder_id from orders)
+du as (
+    select
+        buyer_id,
+        min(coalesce(pay_date, confirm_date)) as pay_date,
+        min(accept_date) as accept_date
+    from dma.current_order
+    where true
+        and buyer_id in (select buyer_id from buyers)
+        and (coalesce(pay_date, confirm_date) <= :last_date or accept_date <= :last_date)
+    group by 1
 )
 select /*+ SYNTACTIC_JOIN*/
  cast(cbcm.create_date as date) as create_date,
@@ -46,34 +67,8 @@ select /*+ SYNTACTIC_JOIN*/
  else false end as has_opened_delivery_wallet,
     cbcm.is_wallet
 from dma.current_billing_cost_mp cbcm
-join /*+jtype(m)*/   order_data co on cbcm.billing_order_ext = co.purchase_ext
+join /*+JTYPE(FM)*/   order_data co on cbcm.billing_order_ext = co.purchase_ext
 left join /*+jtype(h)*/ dma.current_wallet_user cwu on cwu.user_id = co.buyer_id
-left join /*+jtype(h)*/ (
-    select
-        buyer_id,
-        min(coalesce(pay_date, confirm_date)) as pay_date,
-        min(accept_date) as accept_date
-    from dma.current_order
-    where true
-        and buyer_id in (select buyer_id from buyers)
-        and (coalesce(pay_date, confirm_date) <= :last_date or accept_date <= :last_date)
-    group by 1
-) du
-    on du.buyer_id = co.buyer_id
-left join /*+jtype(m)*/ (
-    select /*+ SYNTACTIC_JOIN*/
-        co.purchase_id,
-        co.purchase_ext,
-        MAX(case when expired_date >= date('2022-03-01') and expired_date >= (create_date - interval '5' year) and create_date between status_start_at and coalesce(status_end_at, create_date) then True else False end) as has_avito_bindings,
-        SUM(case when expired_date >= date('2022-03-01') and expired_date >= (create_date - interval '5' year) and create_date between status_start_at and coalesce(status_end_at, create_date) then 1 else 0 end) as cnt_bindings
-    from dma.current_order co
-    left join /*+jtype(h)*/  dma.user_payment_bindings pb on pb.user_id = co.buyer_id
-
-    where true
-        and pb.external_source_provider_id in (18)
-        and pb.is_available = True
-               and co.deliveryorder_id in (select deliveryorder_id from orders)
-	group by 1,2
-) ub 
-	on cbcm.billing_order_ext = ub.purchase_ext
+left join /*+jtype(h)*/ du on du.buyer_id = co.buyer_id
+left join /*+jtype(m)*/ ub on cbcm.billing_order_ext = ub.purchase_ext
 where cast(cbcm.create_date as date) between :first_date and :last_date
