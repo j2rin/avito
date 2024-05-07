11cb219d1597ef4149a5e714ab48ebc45a9a5e37
nnlyakin <nnlyakin@avito.ru>
2024-05-07 16:09:28 +0300

    TNS-16106

---
+++ sources/sql/duplicates_in_dai.sql
@@ -0,0 +1,30 @@
+select
+    event_date,
+    q_items, 
+    all_dups_user,
+    all_dups_cluster,
+    geo_dups_user,
+    geo_dups_cluster,
+    geo_inter_dups_user,
+    geo_inter_strong_dups_user,
+    geo_inter_weak_dups_user,
+    geo_inter_dups_cluster,
+    geo_inter_strong_dups_cluster,
+    geo_inter_weak_dups_cluster,
+    geo_intra_dups_user,
+    geo_intra_strong_dups_user,
+    geo_intra_weak_dups_user,
+    geo_intra_dups_cluster,
+    geo_intra_strong_dups_cluster,
+    geo_intra_weak_dups_cluster,
+    multi_dups_user,
+    multi_strong_dups_user,
+    multi_weak_dups_user,
+    multi_dups_cluster,
+    multi_strong_dups_cluster,
+    multi_weak_dups_cluster,
+    -- Dimensions -----------------------------------------------------------------------------------------------------
+    SPLIT_PART(logical_category, '.', 1) as vertical,
+    logical_category
+from dma.duplicates_in_dai_buckets
+where event_date between :first_date and :last_date
