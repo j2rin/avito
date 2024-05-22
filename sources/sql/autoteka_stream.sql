d4c1caa9953cf691a45f1bd33e39c7d1fac5ebde
Artur Verdyan <agverdyan@avito.ru>
2024-05-22 19:17:35 +0300

    fix gb ROAD-21567

--- sources/sql/autoteka_stream.sql
+++ sources/sql/autoteka_stream.sql
@@ -13,8 +13,6 @@ user_matching as (
 ),
 autoteka as (
 select
-    track_id,
-    event_no,
     event_date,
     cast(event_date as date) as dt,
     cookie_id,
@@ -22,28 +20,19 @@ select
     additionalcookie_id,
     autotekauser_id,
     is_authorized,
-    searchkey,
     searchtype,
     item_id,
-    platenumber,
-    vin,
     autoteka_platform_id,
     autotekaorder_id,
     reports_count,
     amount,
     amount_net,
     payment_method,
-    user_created_at,
     is_new_user,
     funnel_stage_id,
-    utm_campaign,
-    utm_source,
-    utm_medium,
     platform_id,
     autoteka_cookie_id,
     is_pro,
-    utm_content,
-    utm_term,
     'standalone' as source,
   	from_big_endian_64(xxhash64(cast(cast(autotekauser_id as varchar) || 'standalone' as varbinary))) as autoteka_user_hash,
   	from_big_endian_64(xxhash64(cast(cast(autotekaorder_id as varchar) || 'standalone' as varbinary))) autoteka_order_hash,
@@ -56,8 +45,6 @@ where cast(event_date as date) between :first_date and :last_date
 --and event_year between date_trunc('year', :first_date) and date_trunc('year', :last_date) --@trino
 union all
 select
-    track_id,
-    event_no,
     event_date,
     cast(event_date as date) as dt,
     cookie_id,
@@ -65,28 +52,19 @@ select
     null as additionalcookie_id,
     null as autotekauser_id,
     is_authorized,
-    null as searchkey,
     coalesce(search_type, 1) as searchtype,
     item_id,
-    null platenumber,
-    null vin,
     null autoteka_platform_id,
     null as autotekaorder_id,
     reports_count,
     amount,
     amount_net,
     null payment_method,
-    null user_created_at,
     is_new_user,
     funnel_stage_id,
-    null utm_campaign,
-    null utm_source,
-    null utm_medium,
     platform_id,
     null as autoteka_cookie_id,
     is_pro,
-    null utm_content,
-    null utm_term,
     'avito' as source,
   	from_big_endian_64(xxhash64(cast(cast(user_id as varchar) || 'avito' as varbinary))) as autoteka_user_hash,
   	from_big_endian_64(xxhash64(cast(cast(order_items_id as varchar) || 'avito' as varbinary))) as autoteka_order_hash,
@@ -99,51 +77,43 @@ where cast(event_date as date) between :first_date and :last_date
 --and event_year between date_trunc('year', :first_date) and date_trunc('year', :last_date) --@trino
 )
 select
-    autoteka.track_id,
-    autoteka.event_no,
     autoteka.event_date,
     case when autoteka.source = 'standalone' then coalesce(autoteka.cookie_id, ut.cookie_id) else autoteka.cookie_id end as cookie_id,
     case when autoteka.source = 'standalone' then coalesce(ut2.user_id, autoteka.user_id) else autoteka.user_id end as user_id,
     autoteka.additionalcookie_id,
     autoteka.autotekauser_id,
     autoteka.is_authorized,
-    autoteka.searchkey,
     autoteka.searchtype,
     autoteka.item_id,
-    autoteka.platenumber,
-    autoteka.vin,
     autoteka.autoteka_platform_id,
     autoteka.autotekaorder_id,
     autoteka.reports_count,
     autoteka.amount,
     autoteka.amount_net,
     autoteka.payment_method,
-    autoteka.user_created_at,
     autoteka.is_new_user,
     autoteka.funnel_stage_id,
-    autoteka.utm_campaign,
-    autoteka.utm_source,
-    autoteka.utm_medium,
     autoteka.platform_id,
     autoteka.autoteka_cookie_id,
     autoteka.is_pro,
-    autoteka.utm_content,
-    autoteka.utm_term,
     autoteka.source,
     autoteka.fake_user_id,
     autoteka.autoteka_user_hash,
     autoteka.autoteka_order_hash,
-    coalesce(mc.logical_category_id, 24144500001) as logical_category_id,
-    coalesce(mc.vertical_id, 500012) as vertical_id,
+    --coalesce(mc.logical_category_id, 24144500001) as logical_category_id,
+    --coalesce(mc.vertical_id, 500012) as vertical_id,
+    24144500001 as logical_category_id,
+    500012 as vertical_id,
     autoteka.from_block,
     autoteka.action_type,
     autoteka.eid
 from autoteka
-left join dma.current_item ci 
-    on autoteka.item_id = ci.item_id
-left join /*+jtype(h),distrib(l,a)*/ dma.current_microcategories mc
-    on mc.microcat_id = ci.microcat_id
-    and mc.vertical='Transport'
+-- нет времени на оптимизацию, временно отношу все в UsedCars
+-- left join dma.current_item ci 
+--     on autoteka.item_id = ci.item_id
+-- left join /*+jtype(h),distrib(l,a)*/ dma.current_microcategories mc
+--     on mc.microcat_id = ci.microcat_id
+--     and mc.vertical='Transport'
 left join user_matching ut on ut.autoteka_user_id = autoteka.autotekauser_id and autoteka.dt >= ut.date_from and autoteka.dt < ut.lead_date_from
 left join (
     select autoteka_user_id, user_id, date_from, lead_date_from
