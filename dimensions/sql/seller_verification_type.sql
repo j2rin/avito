1df964019bf114194b369d5458df213ab287f594
Kirill Pogozhev <kipogozhev@avito.ru>
2024-05-27 14:44:23 +0300

    VRF-5977 Fix parent dimension

--- dimensions/sql/seller_verification_type.sql
+++ dimensions/sql/seller_verification_type.sql
@@ -1,18 +1,12 @@
-with vrf_types as (
-    select distinct
-            cast(vrf_type as varchar(80)) as value
-    from dma.verification_day
-    where True
-        -- and event_year is not null -- @trino
-        and vrf_type is not NULL
-        and vrf_status = 'verified'
-    union all
-    select
-            cast('not_verified' as varchar(80)) as value
-)
+select distinct
+        vrf_type value
+from dma.verification_day
+where True
+    -- and event_year is not null -- @trino
+    and vrf_type is not NULL
+    and vrf_status = 'verified'
+union all
 select
-        value
-    ,   'is_seller_verified' as parent_dimension
-    ,   value != 'not_verified' as parent_value
-from vrf_types
+        'not_verified' value
 ;
+
