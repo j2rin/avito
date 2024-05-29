27fbf45be7c2ad9d5aefce1961e77cde9538a4a9
Arina Vladimirova <avlerofeeva@avito.ru>
2024-05-29 16:38:51 +0300

    SHOPXDISC-5204

--- sources/sql/avito_sales_events.sql
+++ sources/sql/avito_sales_events.sql
@@ -1,4 +1,9 @@
 select 
-	*
+	event_date,
+	eid,
+	business_platform as platform_id,
+   	user_id,
+   	cookie_id,
+   	from_page
 from dma.avito_sales_events
 where event_date between :first_date and :last_date
