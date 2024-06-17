c589cdb0c71093edd55c540ad2a8ccb1b426bb0e
Mikhail Kudryavtsev <mpkudryavtsev@avito.ru>
2024-06-17 15:14:40 +0300

    add metric HRB-6059

---
+++ sources/sql/haraba_active_user_event.sql
@@ -0,0 +1,12 @@
+select 
+    event_date
+    ,coalesce(platform_name, 'web') as platform
+    ,app_version
+    ,haraba_user_id
+    ,count(1) as events
+from dma.haraba_events
+where event_date between :first_date and :last_date
+      and haraba_user_id is not null
+      -- and event_month between date_trunc('month', :first_date) ) and date_trunc('month', :last_date) -- @trino
+group by 1, 2, 3, 4, 5
+order by 1, 2, 3, 4, 5
