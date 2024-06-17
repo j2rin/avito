7b10dacb8eab060633fc2e5c790e3c655d675aa0
rsklochko <rsklochko@avito.ru>
2024-06-17 17:23:06 +0300

    AL-28904

---
+++ dimensions/sql/buyer_frequency_segmentation.sql
@@ -0,0 +1,19 @@
+select
+    1 as value_id
+    , 'heavy' as value
+union all
+select
+    2 as value_id
+    , 'recent' as value
+union all
+select
+    3 as value_id
+    , 'rare' as value
+union all
+select
+    4 as value_id
+    , 'long rare' as value
+union all
+select
+    5 as value_id
+    , 'reactivated_or_newbie' as value
