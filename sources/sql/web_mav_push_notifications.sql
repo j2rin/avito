2ef97e79509a855640bc49df825b6786590e3d23
Daniil Titov <ddtitov@avito.ru>
2024-06-19 18:01:15 +0300

    BX-30638

--- sources/sql/web_mav_push_notifications.sql
+++ sources/sql/web_mav_push_notifications.sql
@@ -9,3 +9,4 @@ select
 from dma.web_mav_push_notifications
 where true
 	and cast(event_date as date) between :first_date and :last_date
+    -- and event_year between date_trunc('year', :first_date) and date_trunc('year', :last_date) -- @trino
